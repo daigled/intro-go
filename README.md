2fcfb2b3f9b68a23cdf8469be7b7a247deabcd91
Drew Daigle <daigled1@gmail.com>
2023-03-12 18:18:11 -0400

    feat(main): example type conversion
    show example type conversion with float64 and int

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Intro to Go
+This project will follow the O'Reilly book [Learning Go](https://www.oreilly.com/library/view/learning-go/9781492077206/)
--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func exampleTypeConversion() {
+	var x int = 10
+	var y float64 = 30.2
+	// var z float64 = x + y // NB: this will not work because x and y are not the same type; we need to convert
+	var z float64 = float64(x) + y
+	//var d int = x + y // NB2: Same deal here; no automatic type promotion for numeric types in Go
+	var d int = x + int(y)
+	fmt.Println(z)
+	fmt.Println(d)
+}
+
 func main() {
 	fmt.Println("Hello, world")
+
+	exampleTypeConversion()
 }
